6a59710e6c43563a2a19fd00df65bf24fdf42101
jan <j.wandel@ll.olympuscollege.nl>
2022-06-10 22:03:11 +0200

    jup

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import pygame
+import os
 
 # start pygame op
 pygame.init()
 
-scherm = pygame.display.set_mode((800, 800))
+#maakt het scherm en bepaald de groote
+scherm = pygame.display.set_mode((650, 700))
+#naam van de game
+pygame.display.set_caption("Pandora's puzzels")
+
+FPS = 60
+achtergrond = pygame.image.load(os.path.join('textures', 'achtergrond.png'))
+
+def scherm_update():
+    scherm.fill(achtergrond)
+    pygame.display.update()
+
+def main():
+    clock = pygame.time.Clock()
+    aan = True
+    while aan:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                aan = False
+        scherm_update()
+
+if __name__ == "__main__":
+    main()
