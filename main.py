87683fb6f2baf6589dbd0f57e780b8f0fbbe6a87
jan <j.wandel@ll.olympuscollege.nl>
2022-06-18 20:50:11 +0200

    jup

--- main.py
+++ main.py
@@ -3,30 +3,30 @@ import os
 
 pygame.init()
 
-schermBreete = 600
-schermHoogte = 600
+schermBreete = 900
+schermHoogte = 900
 
 scherm = pygame.display.set_mode((schermBreete, schermHoogte))
 pygame.display.set_caption("Pandora's puzzels")
-
-#variabelen
 FPS = 60
-stap = 100
+
+# variabelen
 tegel_groote = 100
 
-
-
-#foto's laden
-kamer = pygame.transform.scale(pygame.image.load(os.path.join('achtergronden', 'kamer1.jpg')), (schermBreete, schermHoogte))
-steen = pygame.image.load(os.path.join('objecten', 'steen.jpg'))
+# foto's laden
 character = pygame.image.load(os.path.join('character', 'c1.jpg'))
+grond = pygame.image.load(os.path.join('objecten', 'grond.jpg'))
+steen = pygame.image.load(os.path.join('objecten', 'steen.jpg'))
+muur = pygame.image.load(os.path.join('objecten', 'muur.jpg'))
+
 
 def draw_grid():
     for line in range(0, 6):
         pygame.draw.line(scherm, (255, 255, 255), (0, line * tegel_groote), (schermBreete, line * tegel_groote))
         pygame.draw.line(scherm, (255, 255, 255), (line * tegel_groote, 0), (line * tegel_groote, schermHoogte))
 
-class Wereld():
+
+class Wereld:
     def __init__(self, info):
         global player_x
         global player_y
@@ -45,6 +45,20 @@ class Wereld():
                     foto_rect.y = player_y * tegel_groote
                     scherm.blit(foto, foto_rect)
 
+                if tegel == 0:
+                    foto = pygame.transform.scale(grond, (tegel_groote, tegel_groote))
+                    foto_rect = foto.get_rect()
+                    foto_rect.x = kolom_aantal * tegel_groote
+                    foto_rect.y = rij_aantal * tegel_groote
+                    scherm.blit(foto, foto_rect)
+
+                if tegel == 1:
+                    foto = pygame.transform.scale(muur, (tegel_groote, tegel_groote))
+                    foto_rect = foto.get_rect()
+                    foto_rect.x = kolom_aantal * tegel_groote
+                    foto_rect.y = rij_aantal * tegel_groote
+                    scherm.blit(foto, foto_rect)
+
                 if tegel == 2:
                     foto = pygame.transform.scale(steen, (tegel_groote, tegel_groote))
                     foto_rect = foto.get_rect()
@@ -55,22 +69,27 @@ class Wereld():
                 kolom_aantal += 1
             rij_aantal += 1
 
-wereld_info =[
-[1, 0, 'Char', 0, 1, 1],
-[1, 0, 0, 0, 0, 1],
-[0, 0, 0, 0, 0, 0],
-[0, 0, 1, 1, 0, 0],
-[0, 0, 0, 2, 0, 0],
-[1, 1, 1, 1, 1, 1],
+
+wereld_info = [
+ [1, 1, 1, 1, 1, 1, 1, 1, 1],
+ [1, 0, 0, 'Char', 0, 0, 0, 0, 1],
+ [1, 0, 0, 0, 0, 0, 0, 0, 1],
+ [1, 0, 0, 2, 0, 0, 0, 0, 1],
+ [1, 0, 0, 0, 0, 0, 0, 0, 1],
+ [1, 0, 0, 0, 0, 2, 0, 0, 1],
+ [1, 0, 0, 0, 0, 0, 0, 0, 1],
+ [1, 0, 0, 0, 0, 0, 0, 0, 1],
+ [1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
 
 wereld = Wereld(wereld_info)
 
+
 def scherm_updaten():
-    scherm.blit(kamer, (0, 0))
     draw_grid()
     Wereld(wereld_info)
     pygame.display.update()
+
 
 def main():
     global player_x
@@ -85,19 +104,43 @@ def main():
                 aan = False
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT and wereld_info[player_y][player_x - 1] == 0:
-                    wereld_info[player_y][player_x] = 0
-                    wereld_info[player_y][player_x - 1] = 'Char'
-                if event.key == pygame.K_RIGHT and wereld_info[player_y][player_x + 1] == 0:
-                    wereld_info[player_y][player_x] = 0
-                    wereld_info[player_y][player_x + 1] = 'Char'
-                if event.key == pygame.K_UP and wereld_info[player_y - 1][player_x] == 0:
-                    wereld_info[player_y][player_x] = 0
-                    wereld_info[player_y - 1][player_x] = 'Char'
-                if event.key == pygame.K_DOWN and wereld_info[player_y + 1][player_x] == 0:
-                    wereld_info[player_y][player_x] = 0
-                    wereld_info[player_y + 1][player_x] = 'Char'
+                if event.key == pygame.K_LEFT:
+                    if wereld_info[player_y][player_x - 1] == 0:
+                        wereld_info[player_y][player_x] = 0
+                        wereld_info[player_y][player_x - 1] = 'Char'
+                    if wereld_info[player_y][player_x - 1] == 2 and wereld_info[player_y][player_x - 2] == 0:
+                        wereld_info[player_y][player_x] = 0
+                        wereld_info[player_y][player_x - 1] = 'Char'
+                        wereld_info[player_y][player_x - 2] = 2
+
+                if event.key == pygame.K_RIGHT:
+                    if wereld_info[player_y][player_x + 1] == 0:
+                        wereld_info[player_y][player_x] = 0
+                        wereld_info[player_y][player_x + 1] = 'Char'
+                    if wereld_info[player_y][player_x + 1] == 2 and wereld_info[player_y][player_x + 2] == 0:
+                        wereld_info[player_y][player_x] = 0
+                        wereld_info[player_y][player_x + 1] = 'Char'
+                        wereld_info[player_y][player_x + 2] = 2
+
+                if event.key == pygame.K_UP:
+                    if wereld_info[player_y - 1][player_x] == 0:
+                        wereld_info[player_y][player_x] = 0
+                        wereld_info[player_y - 1][player_x] = 'Char'
+                    if wereld_info[player_y - 1][player_x] == 2 and wereld_info[player_y - 2][player_x] == 0:
+                        wereld_info[player_y][player_x] = 0
+                        wereld_info[player_y - 1][player_x] = 'Char'
+                        wereld_info[player_y - 2][player_x] = 2
+
+                if event.key == pygame.K_DOWN:
+                    if wereld_info[player_y + 1][player_x] == 0:
+                        wereld_info[player_y][player_x] = 0
+                        wereld_info[player_y + 1][player_x] = 'Char'
+                    if wereld_info[player_y + 1][player_x] == 2 and wereld_info[player_y + 2][player_x] == 0:
+                        wereld_info[player_y][player_x] = 0
+                        wereld_info[player_y + 1][player_x] = 'Char'
+                        wereld_info[player_y + 2][player_x] = 2
 
         scherm_updaten()
+
 
 main()
